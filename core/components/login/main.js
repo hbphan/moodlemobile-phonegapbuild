ec4df7aba310e345a36e2a0a8e21d59757363dcb
hbphan <hbphan@hotmail.com>
2016-09-08 13:36:07 +0700

    add ur

--- core/components/login/main.js
+++ core/components/login/main.js
@@ -51,10 +51,7 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
-	    onEnter: function($state) {
-        $state.go('mm_login.credentials', {siteurl: 'http://courses.airlearn.org'});
-    }
+        controller: 'mmLoginSiteCtrl'	    
     })
 
     .state('mm_login.credentials', {
